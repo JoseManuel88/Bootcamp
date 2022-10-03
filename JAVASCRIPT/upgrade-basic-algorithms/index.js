8b65cd2e7242f0e9f02c67f31c52d70bfcf3c42c
JoseManuel88 <ciruela88@hotmail.es>
2022-10-03 21:51:18 +0200

    "Iteration#6"

--- JAVASCRIPT/upgrade-basic-algorithms/index.js
+++ JAVASCRIPT/upgrade-basic-algorithms/index.js
@@ -57,7 +57,7 @@ console.log(multi);
 const divi = 10 / 2 ;
 console.log(divi);
 // 1.3 Muestra mediante un alert el resto de dividir 15 por 9.
-const resto = 15 & 9;
+const resto = 15 % 9;
 console.log(resto);
 // 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
 // teniendo dos variables y = 10 y z = 5.
@@ -133,4 +133,32 @@ if (number2 / 2 == number1 || number1 / 5 == number3) {
 }
 
 
+// 1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
+for (let i = 0; i <= 9; i++) {
+    console.log(i);
+    
+}
+console.log("- - - - - - - - -")
+// 1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
+// cuando el resto del numero dividido entre 2 sea 0.
 
+for (let i = 0; i <= 9; i++) {
+    if (i % 2 === 0 ) {
+        console.log(i);  
+    }}
+   
+    console.log("- - - - - - - - -")
+// 1.3 Crea un bucle para conseguir dormir contando ovejas. 
+// Este bucle empieza en 0 y termina en 10. 
+// Muestra por consola un mensaje diciendo 'Intentando dormir' en cada vuelta del bucle 
+// y cambia el mensaje en la última vuelta a 'Dormido!'.
+
+
+for (let i = 0; i <= 10; i++) {
+if(i == 10){
+    console.log("Dormido!")
+}else{
+    console.log("Intentando dormir")
+
+}
+}
