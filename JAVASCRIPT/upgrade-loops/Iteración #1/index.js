d864d281f34a85ed888fe4d8d40f2d35a404238a
JoseManuel88 <ciruela88@hotmail.es>
2022-10-07 00:03:16 +0200

    commit loops exercise two

--- JAVASCRIPT/upgrade-loops/Iteración #1/index.js
+++ JAVASCRIPT/upgrade-loops/Iteración #1/index.js
@@ -4,3 +4,13 @@
 
 const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta',
  'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta']
+
+let camisetas = [];
+
+ for (let i = 0; i < products.length; i++) {
+    if (products[i].includes('Camiseta')) {
+        camisetas.push(products[i])
+    }
+    
+ }
+ console.log(camisetas);
