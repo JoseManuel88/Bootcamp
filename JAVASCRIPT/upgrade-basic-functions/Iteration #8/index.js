b56e340fc208df97ad7aeaa35f375609f0c11d11
JoseManuel88 <ciruela88@hotmail.es>
2022-10-06 22:10:10 +0200

    exercise function eight

--- JAVASCRIPT/upgrade-basic-functions/Iteration #8/index.js
+++ JAVASCRIPT/upgrade-basic-functions/Iteration #8/index.js
@@ -2,24 +2,45 @@
 //  una de las palabras que lo conforma.  Puedes usar este array para probar tu función:
 
 const counterWords = [
-    'code',
-    'repeat',
-    'eat',
-    'sleep',
-    'code',
-    'enjoy',
-    'sleep',
-    'code',
-    'enjoy',
-    'upgrade',
-    'code'
-  ];
-  function repeatCounter(param) {
-    // insert code
+  "code",
+  "repeat",
+  "eat",
+  "sleep",
+  "code",
+  "enjoy",
+  "sleep",
+  "code",
+  "enjoy",
+  "upgrade",
+  "code",
+];
+
+// dfsfsejdsañ
+function contadorRepetido(array) {
+  for (let i = 0; i < array.length; i++) {
+    const resultado = array.filter((x) => x === array[i]);
+    console.log("Palabra: " + resultado[0] + ", Contador: " + resultado.length);
   }
-  // function montarEnBici() {
-  //   console.log("Ride or die");
-  //   console.log("nos vemos en vallnord");
-  // console.log("a Full");
-  // };
-  // montarEnBici();
+}
+
+// function contadorRepetidoDistinto(array) {
+//   const array2 = [];
+//   for (let i = 0; i < array.length; i++) {
+//     const resultado = array.filter((x) => x === array[i]);
+
+//     const array2Filtro = array2.filter((x) => x.name === array[i]);
+//     if (array2Filtro.length === 0) {
+//       console.log(
+//         "Palabra: " + resultado[0] + ", Contador: " + resultado.length
+//       );
+//       array2.push({
+//         name: resultado[0],
+//         contador: resultado.length
+//       });
+//     }
+//   }
+// }
+
+contadorRepetido(counterWords);
+// contadorRepetidoDistinto(counterWords);
+
