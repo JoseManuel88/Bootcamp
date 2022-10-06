c0c1af3a4c609c5782a88d8c4a90ea4ea14a8960
JoseManuel88 <ciruela88@hotmail.es>
2022-10-06 12:29:19 +0200

    commit exercise four function

--- JAVASCRIPT/upgrade-basic-functions/Iteración #4/index.js
+++ JAVASCRIPT/upgrade-basic-functions/Iteración #4/index.js
@@ -1,6 +1,14 @@
 // Calcular un promedio es una tarea extremadamente común. Puedes usar este array para probar tu función:
 
 const numbers = [12, 21, 38, 5, 45, 37, 6];
-function average(param) {
-  // insert code
+let summa = average(numbers)
+function average(numbers) {
+  var i = 0, summ = 0, ArrayLen = numbers.length;
+  while (i < ArrayLen) {
+      summ = summ + numbers[i++];
 }
+  return summ;
+}
+// var myArray = [1, 5, 2, 3, 7];
+// var a = average(myArray);
+console.log(summa);
--- JAVASCRIPT/upgrade-basic-functions/Iteración #7/index.js
+++ JAVASCRIPT/upgrade-basic-functions/Iteración #7/index.js
@@ -1,4 +1,5 @@
-// Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array - comprueba si existe el elemento, en caso que existan nos devuelve la posición de dicho 
+// Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe
+//  dentro de dicho array - comprueba si existe el elemento, en caso que existan nos devuelve la posición de dicho 
 // elemento y por la contra un false. Puedes usar este array para probar tu función:
 
 const nameFinder = [
