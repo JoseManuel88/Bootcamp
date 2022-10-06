2a7332f1bd9354fe6506f615fd1f0d7ced4d824e
JoseManuel88 <ciruela88@hotmail.es>
2022-10-06 09:43:51 +0200

    commit java 1

--- JAVASCRIPT/upgrade-basic-functions/Iteración #1/index.js
+++ JAVASCRIPT/upgrade-basic-functions/Iteración #1/index.js
@@ -1,3 +1,15 @@
-function sum(numberOne , numberTwo) {
-    // insert code
+
+// Completa la función que tomando dos números como argumento devuelva el más alto.
+let numberOne = 3;
+let numberTWo = 7;
+
+function sum(numberOne, numberTwo) {
+ if (numberOne > numberTwo) {
+   return numberOne;
   }
+  else {
+   return numberTwo;
+  }
+  // insert code
+}
+console.log(sum(numberOne,numberTWo));
---
+++ JAVASCRIPT/upgrade-basic-functions/Iteración #2/index.js
@@ -0,0 +1,17 @@
+// Completa la función que tomando un array de strings como argumento devuelva el más
+//  largo, en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+
+// Puedes usar este array para probar tu función:
+
+
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+for (let i = 0; i < avengers.lengt i++) {
+  console.log(avengers)
+  
+} {
+ 
+ 
+
+ 
+  // insert code
+}
