c0a4257a350de3f537cd31f89f3372602e095402
JoseManuel88 <ciruela88@hotmail.es>
2022-10-06 20:08:16 +0200

    function exercise 6

--- JAVASCRIPT/upgrade-basic-functions/Iteración #6/index.js
+++ JAVASCRIPT/upgrade-basic-functions/Iteración #6/index.js
@@ -1,20 +1,26 @@
 // Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados,
 //  en caso que existan los elimina para retornar un array sin los elementos duplicados. Puedes usar este array para probar tu función:
 
-
 const duplicates = [
-    'sushi',
-    'pizza',
-    'burger',
-    'potatoe',
-    'pasta',
-    'ice-cream',
-    'pizza',
-    'chicken',
-    'onion rings',
-    'pasta',
-    'soda'
-  ];
-  function removeDuplicates(param) {
-    // insert code
+  "sushi",
+  "pizza",
+  "burger",
+  "potatoe",
+  "pasta",
+  "ice-cream",
+  "pizza",
+  "chicken",
+  "onion rings",
+  "pasta",
+  "soda",
+];
+function removeDuplicates() {
+  const array = [];
+  for (let i = 0; i < duplicates.length; i++) {
+    if (!array.includes(duplicates[i])) {
+      array.push(duplicates[i]);
+    }
   }
+  console.log(array);
+}
+removeDuplicates();
