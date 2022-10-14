17bc10f34a29f9baff6d5ea7adb5004eed6672b0
JoseManuel88 <ciruela88@hotmail.es>
2022-10-14 02:02:44 +0200

    commit the commit two asincro

--- JAVASCRIPT/upgrade-asynchronous/Iteración-1/fetch.js
+++ JAVASCRIPT/upgrade-asynchronous/Iteración-1/fetch.js
@@ -23,7 +23,7 @@ const fetchData = () =>{
     .then((data) => console.log(data))
 }
 
-button$$.addEventListener("click", (event) => fetchData(event);)
+button$$.addEventListener("click", (event) => fetchData(event));
 // 1.3 En base al ejercicio anterior. Crea dinamicamente un elemento  por cada petición 
 // a la api que diga...'El nombre X tiene un Y porciento de ser de Z' etc etc.
 // EJ: El nombre Pepe tiene un 22 porciento de ser de ET y un 6 porciento de ser 
